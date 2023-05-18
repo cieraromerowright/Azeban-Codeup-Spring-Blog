b7928ef30d990333698aaa55727145b2a9690495
Ciera Romero-Wright <romerowrightciera@gmail.com>
2023-05-17 23:51:15 -0500

    done

---
+++ src/main/java/controller/MathController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+public class MathController {
+    @GetMapping("/add/{num1}/and/{num2}")
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    public int subtract(@PathVariable int num1, @PathVariable int num2) {
+        return num2 - num1;
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    public int multiply(@PathVariable int num1, @PathVariable int num2) {
+        return num1 * num2;
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    public double divide(@PathVariable double num1, @PathVariable double num2) {
+        return num1 / num2;
+    }
+}
