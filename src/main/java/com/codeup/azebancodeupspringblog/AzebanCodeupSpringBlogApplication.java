2c73da362524a7126e38036654b471b6ec2ace69
Ciera Romero-Wright <romerowrightciera@gmail.com>
2023-05-23 10:41:34 -0500

    controllers

---
+++ src/main/java/com/codeup/azebancodeupspringblog/AzebanCodeupSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.azebancodeupspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzebanCodeupSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzebanCodeupSpringBlogApplication.class, args);
+    }
+
+}
